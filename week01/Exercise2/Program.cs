1caed4fcdd4e816f884dc62cebb9fb3648e0a610
Dornob1 <michael.oelschig@gmail.com>
2025-05-10 16:10:12 +0200

    Completed excercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+        string letter = "";
+
+// To determine grade letter value
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"{letter}");
+
+// To determine if student passed
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You pass!");
+        }
+        else
+        {
+            Console.Write("You fail. Better luck next time!");
+        }
     }
 }
