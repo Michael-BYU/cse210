18d2ed2def2f87db2e2d7f798cb0bd06e974775f
Dornob1 <michael.oelschig@gmail.com>
2025-05-10 22:40:37 +0200

    Completed excercise

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Formats.Asn1;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Welcome();
+
+        string name = UserName();
+        int number = UserNumber();
+        int squaredNumber = Square(number);
+        
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome!");
+        }
+        static string UserName()
+        {
+            Console.WriteLine("Enter username");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int UserNumber()
+        {
+            Console.WriteLine("What is your favourite number?");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static int Square(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void Result(string name, int square)
+        {
+            Console.WriteLine($"{name}, your square root is {square}");
+        }
     }
 }
