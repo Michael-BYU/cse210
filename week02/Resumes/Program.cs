f3f303bcc1990cb2c0aa225ee9e480ddca1cff8c
Dornob1 <michael.oelschig@gmail.com>
2025-05-15 19:51:58 +0200

    Completed excercise

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Amazon";
+        job1._jobTitle = "CEO";
+        job1._startYear = 2004;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+        job2._company = " Microcenter";
+        job2._jobTitle = "Web Developer";
+        job2._startYear = 1998;
+        job2._endYear = 2004;
+
+        Resume resume = new Resume();
+        resume._jobList.Add(job1);
+        resume._jobList.Add(job2);
+
+        resume.DisplayDetails();
     }
 }
